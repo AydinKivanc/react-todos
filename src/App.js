403a11599a677937e2c1f0012d0225b971f4aa3f
AydinKivanc <aydinkivanc@gmail.com>
2022-08-31 15:04:07 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import React, { useState } from 'react';
 
 function App() {
+
+  const [todoText, setTodoText] = useState("");
+
+
   return (
-    <div>
-      Hello
+    <div className="container">
+      <h1 className="text-center my-5">Todo App</h1>
+      <div class="input-group mb-3">
+        <input
+          type={todoText}
+          class="form-control"
+          placeholder="Type your todo"
+          onChange={(event) => setTodoText(event.target.value)}     
+        />
+        <button
+          class="btn btn-primary btn-block"
+          type="submit"                                             // form icindeki button a submit olarak type verdik.
+          id="button-addon2"
+        >
+          ADD
+        </button>
+      </div>
     </div>
   );
 }
