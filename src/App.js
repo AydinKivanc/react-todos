2f5c17c5644a2694b8f7a456002deb4ee4af5998
AydinKivanc <aydinkivanc@gmail.com>
2022-09-02 01:24:44 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,40 @@ function App() {
   const [todos, setTodos] = useState([]); // listelenen todosların state i
 
   const handleSubmit = (event) => {
-    event.preventDefault();              // prevent default formun defaultu submit ile gonderme islemini durdurur
+    event.preventDefault(); // prevent default formun defaultu submit ile gonderme islemini durdurur
     if (todoText === "") {
       alert("Please enter a todo");
       return;
     }
-    const newTodo = {                   // yeni todolari obje olarak oluşturur
+    const newTodo = {
+      // yeni todolari obje olarak oluşturur
       id: new Date().getTime(),
       isDone: false,
-      text: todoText,                   // assagiya todos.map icinde {item.text} olarak todoText gider
+      text: todoText, // assagiya todos.map icinde {item.text} olarak todoText gider
       date: new Date(),
     };
-    setTodos([...todos, newTodo]);      // yeni todo onceki todos larla beraber setTodos a eklenir
+    setTodos([...todos, newTodo]); // yeni todo onceki todos larla beraber setTodos a eklenir. Spread operator
+    setTodoText(""); // form input daki text in state i bosaltir. Input icinde yazi kalmaz.
     //console.log(newTodo);
+  };
+
+  const changeIsDone = (id) => {
+    // eklenmis olan todosların isDone degerini degistiren buttona verilen fonksiyondur.
+    //console.log(id);                    // button a tiklandiginda o todo nun id si gelir
+    const searchedTodo = todos.find((item) => item.id === id); // item in id si ile button a click oldugunda alinan id ye esit olani todo yu
+    const updatedTodo = { ...searchedTodo, isDone: !searchedTodo.isDone }; // bulunan todo nun isDone degerini tersi olarak degistirir.
+    // searchedTodo ile bulunan todo nun tum ozellikleri ile yeni bir obje olusturuyoruz
+    //...searchedTodo spread operator kullanarak tum ozellikler yeniye gecirilir. Bu yeni obje isDone degeri eskisinin terse cevrili olarak olusur.
+
+    //console.log(updatedTodo);
+    //console.log(searchedTodo);
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    // searchedTodo da id esit olani alip ondan updatedTodo ile yeni bir todo olusturduk. Eski olani todos icinden cikarmak icin
+    // filteredTodos ile esit olmayanlarin tumunu cagirip yeni bir dizi olustururuz filtre ederiz.
+    // Bylelikle isDone degeri degismemis olan eski todo yu todos dizisinden cikarmis oluruz.
+    // Sonrasinda updatedTodo ile yeni olusturulmus obje todos a eklenecek
+    setTodos([...filteredTodos, updatedTodo]);
+    console.log(filteredTodos);
   };
 
   return (
@@ -41,19 +62,27 @@ function App() {
           </button>
         </div>
       </form>
-      {todos.lenght <= 0 ? (             // indexi 0 a esit veya kucukse ? () degilse : () yapar.
-        <p className= "text-center my-5">You don't have any todos</p>  // .lenght <= ? ()  : ()    bu if yapisini genelde JSX de kullaniriz
+      {todos.lenght <= 0 ? ( // indexi 0 a esit veya kucukse ? () degilse : () yapar.
+        <p className="text-center my-5">You don't have any todos</p> // .lenght <= ? ()  : ()    bu if yapisini genelde JSX de kullaniriz
       ) : (
         <>
-        {
-          todos.map(item => {           // suslu parantez yaparsak return yazariz ve jsx doneriz.    .map(item => ()) bu sekilde yazarsak () kendisi return islemidir 
-            return(
-              <div className="alert alert-secondary" role="alert">
-              {item.text} 
-            </div> 
-            )
-          })
-        }
+          {todos.map((item) => {
+            // suslu parantez yaparsak return yazariz ve jsx doneriz.    .map(item => ()) bu sekilde yazarsak () kendisi return islemidir
+            return (
+              <div
+                className="alert alert-secondary d-flex justify-content-between align-items-center"
+                role="alert"
+              >
+                {item.text}
+                <button
+                  onClick={() => changeIsDone(item.id)}
+                  className="btn btn-sm btn-secondary"
+                >
+                  {item.isDone === false ? "Done" : "Undone"}
+                </button>
+              </div>
+            );
+          })}
         </>
       )}
     </div>
